8db56fbecaa4d2fbc5520ef2a96d2ce5d02f2313
Rahul19241 <RAHULPALAHWAT@GMAIL.COM>
2021-11-07 20:39:03 +0530

    Added background music

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ let music_back=new Audio("pubg_theme_song.mp3")
 let audioturn=new Audio("pubg_awm_notification.mp3")
 let gameover=new Audio("applause.mp3")
 
-music_back.loop=true;
-music_back.play();
-
 let turn="X"
 let isgameover=false;
 
@@ -41,6 +38,7 @@ Array.from(boxes).forEach(element =>{
     let boxtext=element.querySelector('.boxtext');
     element.addEventListener('click',()=>{
         if(boxtext.innerText===''){
+            music_back.play();
             boxtext.innerText=turn;
             turn=changeTurn();
             audioturn.play();
@@ -65,3 +63,6 @@ reset.addEventListener('click', ()=>{
     document.getElementsByClassName("info")[0].innerText="Turn for" +turn;
     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width="0%"
 })
+
+
+
